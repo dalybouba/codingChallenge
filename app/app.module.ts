a6143e607e2639b2552c0d3a9ec5449ed5ae102e
mboubakerArionTech <simorghconsulting90@gmail.com>
2021-04-18 01:30:08 +0200

    declare routing module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from "./app.component";
 import { AddComponent } from "./add/add.component";
 import { UpdateComponent } from "./update/update.component";
 import { ReadComponent } from "./read/read.component";
+import { AppRoutingModule } from "./app-routing.module";
 @NgModule({
 imports:[
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
+    AppRoutingModule,
     UpgradeModule
 ],
 declarations :[
